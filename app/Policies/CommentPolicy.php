13dd45a32dbe4f9df36d5c314d58416182ab6f91
andrii <andrii.zharkov@zfort.com>
2024-12-07 22:56:57 -0700

    added comment policy with delay 10 minutes

---
+++ app/Policies/CommentPolicy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CommentPolicy
+{
+    public function update(User $user, Comment $comment): bool
+    {
+        return $comment->user_id === $user->id && $comment->created_at->diffInMinutes(now()) <= 10;
+    }
+}
